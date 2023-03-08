ced88b6b419e7bb01b214c164241290eb972aad5
TeriyakiGod <kacperochnik01@gmail.com>
2023-03-09 00:00:56 +0100

    Death sprite fix, little level fixes

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using TMPro;
 using Random = UnityEngine.Random;
 
 public class Coins : MonoBehaviour
@@ -8,16 +7,12 @@ public class Coins : MonoBehaviour
     public float speed = 3f;
     public float offset = .2f;
     float startPositionY;
-    TextMeshProUGUI text;
-
     void Start()
     {
         startPositionY = transform.position.y;
         Animator anim = GetComponent<Animator>();
         AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
         anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
-        text = GetComponent<TextMeshProUGUI>();
-
     }
 
     void Update()
